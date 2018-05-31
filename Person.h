cd03ba2107e60592a16c14ed1a31ea33898e86e8
Tibebeselasie Mehari <37696850+tibebeselasie-mehari@users.noreply.github.com>
2018-06-01 00:35:00 +0300

    initial commit

---
+++ Person.h
@@ -0,0 +1,7 @@
+class Person
+{
+    private: string name;
+    public:
+        Person(string name);
+        string getName();
+};
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Person.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to the program" << endl;
+    Person person("sfsda");
+    cout << person.getName();
+    return 0;
+} 
